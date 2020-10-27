d1bd5d75a5d7637db034a2e52e03503e458c15fd
Remi Douah <remidouah@gmail.com>
2020-10-27 12:10:36 -0500

    Odd game

--- OddOrEven/Program.cs
+++ OddOrEven/Program.cs
@@ -6,7 +6,30 @@ namespace OddOrEven
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IsOdd();
         }
+
+
+        public static bool IsOdd()
+        {
+            int num;
+            Console.WriteLine("Enter a number of your choice" );
+              num = int.Parse(Console.ReadLine());
+
+            if (num % 3 == 0)
+            {
+
+                Console.WriteLine("you got the right answer and you are so smart");
+                return true;
+            }
+
+            else
+            {
+
+                Console.WriteLine("sorry, you got the wrong answer, try again!");
+                return false;
+            }
+        }
+
     }
 }
